4b8af08f8212dede3f2c46a9e6203190653cbb70
Tyrant2033 <durbinwyatt3@gmail.com>
2024-04-05 11:05:02 -0400

    Refactoring model location

--- update_scheduler.py
+++ update_scheduler.py
@@ -1,3 +1,5 @@
+import os
+
 import gdown
 
 
@@ -5,6 +7,8 @@ import gdown
 def download_file_from_google_drive():
     file_id = "1OuWtu2ojLQLdxnzMKm452rfaVH3crEWA"
     destination = "./models"
+    if not os.path.exists(destination):
+        os.makedirs(destination)
 
     url = f"https://drive.google.com/uc?id={file_id}"
     gdown.download(url, destination, quiet=False)
